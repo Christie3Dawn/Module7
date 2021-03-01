91181e4c6367548497d0935af0b592b29ad8de57
Christie3Dawn <79064441+Christie3Dawn@users.noreply.github.com>
2021-02-28 19:31:24 -0500

    Completed assignment

---
+++ Mod7_Assignment.py
@@ -0,0 +1,97 @@
+# 1. Try the code below and revise it to current time.
+
+from datetime import datetime
+from datetime import timedelta
+import time
+import sys
+
+now = datetime.today()
+print("The current date and time is: ", now)
+
+print("The current date and time is: {0}/{1}/{2}\t{3}:{4}".format(now.month, now.day, now.year, now.hour, now.minute))
+
+
+# Using given code
+import sys
+import datetime
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+
+    now = datetime.datetime.now()   # Local Time (in a datetime.datetime object)
+
+    if len(data) == 4:
+        store, item, cost, payment = data
+        print("{0}\t{1}\t{2}".format(now.strftime("%m/%d/%Y %I:%M:%S %p"), item, cost))
+
+
+#import sys
+
+#for line in sys.stdin:
+#    data = line.strip().split("\t")
+#
+#    if len(data) == 6:
+#       date, time, store, item, cost, payment = data
+#       print("{0}\t{1}\t{2}".format(item, cost, time))
+
+
+#### From Dr. Friedman ###
+
+#import sys
+import datetime
+
+#data = datetime.datetime.now().time()
+#print("current time: ")
+#print(data)
+#print("")
+
+
+# 2.  Add the timedelta to the datetime and subtract 60 second and added 2 year .
+#     (Hit: timedelta(seconds=60))  For each condition, state the code and output.
+
+print("Adjusted date time (add 2 years, minus 60 sec) is: ", now + timedelta(weeks = 104, seconds = -60))
+
+
+
+
+from datetime import timedelta
+#add 1 day
+
+
+# 3. Create a timedelta object representing 100 days, 10 hours, and 13 minutes.
+from datetime import timedelta
+
+d = timedelta(microseconds = -1)
+
+(d.days, d.seconds, d.microseconds)
+
+new = timedelta(days = 100, hours = 10, minutes = 13)
+print("Timedelta representing 100 days, 10 hours, and 13 minutes: ", new)
+
+
+
+
+# 4. Write a function that takes two arguments (feet and inches) with this time object
+
+#from datetime import datetime
+# get current date
+
+#datetime_object = datetime.now()
+
+#print(datetime_object)
+
+#print('Type: - ', type(datetime_object))
+
+from datetime import datetime
+from datetime import timedelta
+
+def distance():
+    print()
+    feet, inches = eval(input("Enter your distance in (feet, inches): "))
+    cfeet = inches * 12
+    dfeet = feet + cfeet
+    dtimemin = round(dfeet/272.8, 2)
+    dtime = datetime.today() + timedelta(minutes = dtimemin)
+    print("You will arrive at {0}:{1} walking that distance.".format(dtime.hour, dtime.minute))
+
+distance()
